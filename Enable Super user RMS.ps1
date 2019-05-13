b55fccf512a1713467c5ebffcdd7d6e113c690ab
Antonino <36573756+agressini@users.noreply.github.com>
2019-05-13 11:50:43 -0300

    commit

--- Enable Super user RMS.ps1
+++ Enable Super user RMS.ps1
@@ -1,4 +1,9 @@
-##ENABLE RMS Super User
+Connect-AadrmService
+(Get-Module aadrm -ListAvailable).Version
+Get-AadrmConfiguration
+Get-AadrmKeys
 Get-AadrmSuperUserFeature
+Get-AadrmSuperUserGroup
+Get-AadrmSuperUser
 ## si no esta habilitado se habilita getEnable-AadrmSuperUserFeature
 Add-AadrmSuperUser -EmailAddress "XXXXXXXX@tecpetrol.com"
