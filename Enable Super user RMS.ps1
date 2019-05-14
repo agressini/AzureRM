fe6da32957c3031f44720cffcca41b91857b2d06
Antonino <36573756+agressini@users.noreply.github.com>
2019-05-14 10:08:55 -0300

    su en Rms

--- Enable Super user RMS.ps1
+++ Enable Super user RMS.ps1
@@ -1,9 +1,9 @@
 Connect-AadrmService
 (Get-Module aadrm -ListAvailable).Version
 Get-AadrmConfiguration
-Get-AadrmKeys
 Get-AadrmSuperUserFeature
 Get-AadrmSuperUserGroup
 Get-AadrmSuperUser
 ## si no esta habilitado se habilita getEnable-AadrmSuperUserFeature
-Add-AadrmSuperUser -EmailAddress "XXXXXXXX@tecpetrol.com"
+## Add-AadrmSuperUser -EmailAddress "AzureRMS-SUG@tecpetrol.com"
+Set-AadrmSuperUserGroup -GroupEmailAddress "AzureRMS-SUG@tecpetrol.com"
